f7cd11554e1eae60d86235eb757caa039b81db65
Nick Kinney <kinneyanick@gmail.com>
2020-04-13 20:55:21 -0500

    Potential Compilation method

---
+++ src/server/Poker++_Server.cpp
@@ -0,0 +1,28 @@
+// 1000845009	Mathew Zinke
+
+/***********************************************************************************************************
+*
+*	created by: MPZinke
+*	on ..
+*
+*	DESCRIPTION:
+*	BUGS:
+*	FUTURE:
+*
+***********************************************************************************************************/
+#include<iostream>
+
+#include"DISPLAY.h"
+#include"GLOBAL.h"
+
+
+
+int main(int argc, char* argv[])
+{
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "Poker++");
+	DISPLAY main_window(4);
+
+	app->run(main_window);
+	
+	return 0;
+}
