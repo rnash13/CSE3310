b2be904605f29384bff38614121f25309f3c17ec
mpzinke <mpzinke>
2020-04-15 12:56:12 -0500

    Initial skeleton of classes

---
+++ src/server/Test.cpp
@@ -0,0 +1,34 @@
+
+/***********************************************************************************************************
+*
+*	created by: MPZinke
+*	on ..
+*
+*	DESCRIPTION: TEMPLATE
+*	BUGS:
+*	FUTURE:
+*
+***********************************************************************************************************/
+
+#include<iostream>
+#include"CARD.h"
+#include"DECK.h"
+#include"HAND.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	DECK new_deck;
+	for(int x = 0; x < 52; x++) 
+	{
+		CARD tmp = new_deck.draw_card();
+		std::stringstream str;
+		str << tmp;
+		std::cout << str.str() << std::endl;
+	}
+
+	return 0;
+}
