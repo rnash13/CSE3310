bb23edf5a2475aa1e589481808f90d776620aab9
Nick Kinney <kinneyanick@gmail.com>
2020-04-16 10:36:18 -0500

    Added GAME_SERVER header

---
+++ include/GAME_SERVER.h
@@ -0,0 +1,13 @@
+#ifndef GAME_SERVER_inc
+#define GAME_SERVER_inc
+
+#include "chat_server.h"
+#include "asio.hpp"
+
+class GAME_SERVER: public chat_server {
+    public:
+        GAME_SERVER(asio::io_context& io_context, const tcp::endpoint& endpoint);
+        void loop();
+};
+
+#endif
