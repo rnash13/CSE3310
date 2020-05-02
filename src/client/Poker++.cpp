e0043ac4e7c4771cf42a56252cd0834bf36dbc6f
Nick Kinney <kinneyanick@gmail.com>
2020-05-01 20:27:46 -0500

    Reworked client main file, now has shared stringstream that receives all input from server, need to implement timeout function in gtkmm to read the buffer

--- src/client/Poker++.cpp
+++ src/client/Poker++.cpp
@@ -12,6 +12,7 @@
 ***********************************************************************************************************/
 #include<iostream>
 #include<thread>
+#include<sstream>
 
 #include"DISPLAY.h"
 #include"GLOBAL.h"
@@ -20,46 +21,48 @@
 
 int main(int argc, char* argv[])
 {
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <host> <port>\n";
+        return 1;
+    }
     try
     {
+        std::thread client;
+        std::thread send;
+        std::thread gui;
         
-
-        if (argc != 3)
-        {
-            std::cerr << "Usage: " << argv[0] << " <host> <port>\n";
-            return 1;
-        }
+        std::stringstream inbuffer;
 
         asio::io_context io_context;
 
         tcp::resolver resolver(io_context);
         auto endpoints = resolver.resolve(argv[1], argv[2]);
-        chat_client c(io_context, endpoints);
+        chat_client c(io_context, endpoints, inbuffer);
 
-        std::thread t([&io_context]() {
-            io_context.run();
+        send = std::thread([&io_context]() {io_context.run();});
+        client = std::thread([&] () -> void {        
+            char line[chat_message::max_body_length + 1];
+            while (std::cin.getline(line, chat_message::max_body_length + 1))
+            {
+                chat_message msg;
+                msg.body_length(std::strlen(line));
+                std::memcpy(msg.body(), line, msg.body_length());
+                msg.encode_header();
+                c.write(msg);
+            }
+            c.close();
         });
-        
-        char line[chat_message::max_body_length + 1];
-        std::thread inp([&]() {
-        while (std::cin.getline(line, chat_message::max_body_length + 1))
-        {
-            chat_message msg;
-            msg.body_length(std::strlen(line));
-            std::memcpy(msg.body(), line, msg.body_length());
-            msg.encode_header();
-            c.write(msg);
-        }
+        gui = std::thread([&] () -> void { 
+            Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("org.gtkmm.poker");
+            DISPLAY main_window(4);
+
+            app->run(main_window);
+
         });
-        
-        Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("org.gtkmm.poker");
-        DISPLAY main_window(4);
-
-        app->run(main_window);
-
-        inp.join();
-        c.close();
-        t.join();
+        send.join();
+        client.join();
+        gui.join();
     }
     catch (std::exception& e)
     {
